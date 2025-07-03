5b74de5c1702749d4a99654a7e070baaabd32f44
Pranshu Tiwari <pranshutiwari560@gmail.com>
2025-07-04 00:28:08 +0530

    authentication

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,6 @@
 const userModel = require('../model/userSchema');
 const emailValidator = require("email-validator");
+const bcrypt = require('bcrypt');
 
 const signup = async (req,res,next) => {
   const { name ,email, password ,confirmPassword} = req.body;
@@ -70,7 +71,9 @@ const signin = async (req, res) => {
     email
   })
   .select('+password');
-  if (!user || user.password !== password){
+
+
+  if (!user || !(await bcrypt.compare( password, user.password ) )){
     return res.status(400).json({
     success :false,
     message: 'invalid credentials'
@@ -100,5 +103,49 @@ const signin = async (req, res) => {
 
  }
 }
+
+
+const  getUser = async (req, res, next) => {
+  const userId = req.user.id;
+
+  try{
+       const user = await userModel.findById(userId);
+       return res.status(200).json({
+        success:true,
+        data:user
+       })
+       
+  }catch(e){
+    return res.status(400)
+.json({
+  success :false,
+  message:e.message
+})
+  }
+}
+
+const logout = (req,res) => {
+  try {
+    const cookieOption ={
+      expires: new Date(),
+      httpOnly: true
+
+    };
+    res.cookie("token", null ,cookieOption);
+    res.status(200).json({
+      success:true,
+      meassage:' logout successfully'
+    })
+
+  }catch(e){
+    res.status(400).json({
+    success: false,
+    message: e.message
+  })
+
+  }
+
+}
  
-module.exports =  {signup,signin}
+module.exports =  {
+  signup,signin,getUser,logout}
--- router/authRoute.js
+++ router/authRoute.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const authRouter = express.Router();
-const { signup ,signin } = require('../controller/authController'); 
+const { signup ,signin ,getUser ,logout} = require('../controller/authController'); 
+const jwtAuth = require('../middleware/jwtAuth');
 
 authRouter.post('/signup', signup);
 authRouter.post('/signin', signin);
+authRouter.post('/user',jwtAuth, getUser);
+authRouter.post('/logout', jwtAuth, logout);
 
 
 module.exports = authRouter;
